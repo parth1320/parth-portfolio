d5aab41d73a145ad10693da8bb01e7a358cc0b0a
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-06-07 22:53:26 +0200

    hero section done

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Bio } from "../../data/constants";
+import Typewriter from "typewriter-effect";
+import HeroImg from "../../Images/profile-pic-1.png";
+import HeroBgAnimation from "../HeroBganimation";
+import {
+  HeroBg,
+  HeroContainer,
+  HeroInnerContainer,
+  HeroLeftContainer,
+  HeroRightContainer,
+  TextLoop,
+  Img,
+  ResumeButton,
+  Span,
+  SubTitle,
+  Title,
+} from "./HeroSectionStyledComponent";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>
+              Hi, I am <br /> {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume
+            </ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
 };
 
 export default HeroSection;
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { skills } from "../../data/constants";
+import styled from "styled-components";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  SkillsContainer,
+  Skill,
+  SkillTitle,
+  SkillImage,
+  SkillItem,
+  SkillList,
+} from "./SkillsStyledComponent";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc>
+          Here are some of my skills on which I have been working on for the
+          past 2 years.
+        </Desc>
+        <SkillsContainer>
+          {skills.map((skill) => (
+            <Skill>
+              <SkillTitle>{skill.title}</SkillTitle>
+              <SkillList>
+                {skill.skills.map((item) => (
+                  <SkillItem>
+                    <SkillImage src={item.image} />
+                    {item.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          ))}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Skills;
